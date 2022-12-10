a32dad3106fe94a4997c73d354b4b497facceffc
Ihsan Akbay <ihsanakbay0@gmail.com>
2022-12-10 21:12:02 +0300

    Initial Commit

---
+++ GenericNetworkLayerExample/GenericNetworkLayerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericNetworkLayerExampleApp.swift
+//  GenericNetworkLayerExample
+//
+//  Created by İhsan Akbay on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GenericNetworkLayerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
